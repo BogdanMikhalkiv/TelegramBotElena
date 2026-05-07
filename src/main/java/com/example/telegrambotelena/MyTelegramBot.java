2fc0e65aeff2da8e4736156c720054d28cc57c8c
BogdanMikhalkiv <bogdan.mikhalkiv1@gmail.com>
2026-05-07 12:56:12 +0200

    removed sensitive data again

--- src/main/java/com/example/telegrambotelena/MyTelegramBot.java
+++ src/main/java/com/example/telegrambotelena/MyTelegramBot.java
@@ -1,18 +1,28 @@
 package com.example.telegrambotelena;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 
 @Component
-@AllArgsConstructor
 public class MyTelegramBot  implements SpringLongPollingBot {
 
     private final UpdateConsumer updateConsumer;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    public MyTelegramBot(UpdateConsumer updateConsumer, @Value("${bot.token}") String botToken) {
+        this.updateConsumer = updateConsumer;
+        this.botToken = botToken;
+    }
+
     @Override
     public String getBotToken() {
-        return "8611415969:AAG62hlCyuW6PZqlPaa0IsE9zfI08igNS8c";
+        return botToken;
     }
 
     @Override
