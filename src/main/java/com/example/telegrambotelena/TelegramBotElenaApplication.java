4ded4f8d03640526e68fe0d8ed149fc155daa59a
BogdanMikhalkiv <bogdan.mikhalkiv1@gmail.com>
2026-05-07 11:29:35 +0200

    test

---
+++ src/main/java/com/example/telegrambotelena/TelegramBotElenaApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotelena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotElenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotElenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegrambotelena/TelegramBotElenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrambotelena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotElenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
